d629455facf986c309b33933edf85750adba129d
wc-dev <chan.wilson.wc@gmail.com>
2023-05-21 15:10:07 -0700

    feat: added basic sentiment analysis of overall score

--- app/sentiment_analysis.py
+++ app/sentiment_analysis.py
@@ -1,12 +1,43 @@
 import nltk
-from nltk.sentiment import SentimentIntensityAnalyzer
+from nltk.corpus import wordnet as wn
+from nltk.corpus import sentiwordnet as swn
+from nltk.tag import pos_tag
+from nltk.stem import WordNetLemmatizer
 
+nltk.download('sentiwordnet')
+nltk.download('wordnet')
 
-def getSentimentBasic(comments:list[str]):
-    sia = SentimentIntensityAnalyzer()
-    for comment in comments:
-        score = sia.polarity_scores(comment)
-        print(score)
+def penn_to_wn(tag):
+    if tag.startswith('N'):
+        return wn.NOUN
+    elif tag.startswith('V'):
+        return wn.VERB
+    elif tag.startswith('R'):
+        return wn.ADV
+    elif tag.startswith('J'):
+        return wn.ADJ
+    return None
+
+def analyzeSentiment(sentence: str) -> float:
+    sentiment = 0.0
+    token = nltk.word_tokenize(sentence)
+    after_tagging = pos_tag(token)
+    lemmatizer = WordNetLemmatizer()
+    for word, tag in after_tagging:
+        wn_tag = penn_to_wn(tag)
+        if wn_tag not in (wn.NOUN, wn.ADJ, wn.ADV):
+            continue
+        lemma = lemmatizer.lemmatize(word, pos=wn_tag)
+        if not lemma:
+            continue
+        synsets = wn.synsets(lemma, pos=wn_tag)
+        if not synsets:
+            continue
+        synset = synsets[0]     # take the first sense, the most common
+        swn_synset = swn.senti_synset(synset.name())
+        sentiment += swn_synset.pos_score() - swn_synset.neg_score() # ignore error, code outputs correclty
+
+    return sentiment
 
 
 sentences = ["VADER is smart, handsome, and funny.",
@@ -16,4 +47,6 @@ sentences = ["VADER is smart, handsome, and funny.",
              "VADER is VERY SMART, handsome, and FUNNY!!!",
              "VADER is VERY SMART, really handsome, and INCREDIBLY FUNNY!!!"]
 
-print(getSentimentBasic(sentences))
+for sentence in sentences:
+    print(analyzeSentiment(sentence))
+
