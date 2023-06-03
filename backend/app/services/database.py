3fd76e58742803d9374ad15a5e6f2ec165fbd09e
wc-dev <chan.wilson.wc@gmail.com>
2023-06-03 01:42:30 -0700

    feat: added error handling

--- backend/app/services/database.py
+++ backend/app/services/database.py
@@ -13,35 +13,69 @@ class DatabaseHandler:
     def __init__(self, database_instance):
         self.db = database_instance
 
-    def call_stored_posts(self, PostInformation):
+    def call_store_post(
+        self, PostInformation, compound, neg, neu, pos, summation_score
+    ):
         cursor = self.db.cursor()
-        cursor.callproc(
-            "reddit.store_post",
-            (
-                PostInformation.title,
-                PostInformation.permalink,
-                PostInformation.submission_id,
-                PostInformation.upvote_ratio,
-                PostInformation.post_timestamp,
-            ),
-        )
-        self.db.commit()
+        try:
+            cursor.execute(
+                "CALL reddit.store_post(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                (
+                    PostInformation.title,
+                    PostInformation.permalink,
+                    PostInformation.submission_id,
+                    PostInformation.selftext,
+                    compound,
+                    neg,
+                    neu,
+                    pos,
+                    summation_score,
+                    PostInformation.upvote_ratio,
+                    PostInformation.post_timestamp,
+                ),
+            )
+        except (Exception, psycopg2.Error) as error:
+            print("Error while inserting data into table", error)
+        finally:
+            self.db.commit()
+            if cursor:
+                cursor.close()
+
+    def call_get_post(self, submission_id: str):
+        cursor = self.db.cursor(cursor_factory=extras.RealDictRow)
+        res = {}
+        try:
+            cursor.execute("CALL reddit.get_post(%s)", submission_id)
+            res = cursor.fetchall()
+        except (Exception, psycopg2.Error) as error:
+            print("error fetching post", error)
+        finally:
+            if cursor:
+                cursor.close()
+
+        return res
 
     def call_store_comments(
         self, submission, permalink, parent_id, score, comment_timestamp
     ):
         cursor = self.db.cursor()
-        cursor.execute(
-            "CALL reddit.store_comment(%s, %s, %s, %s, %s)",
-            (
-                submission,
-                permalink,
-                parent_id,
-                score,
-                comment_timestamp,
-            ),
-        )
-        self.db.commit()
+        try:
+            cursor.execute(
+                "CALL reddit.store_comment(%s, %s, %s, %s, %s)",
+                (
+                    submission,
+                    permalink,
+                    parent_id,
+                    score,
+                    comment_timestamp,
+                ),
+            )
+        except (Exception, psycopg2.Error) as error:
+            print("error while inserting data into reddit.comments", error)
+        finally:
+            self.db.commit()
+            if cursor:
+                cursor.close()
 
     def call_store_comment_sentiment(
         self, comment_id, compound, neg, neu, pos, summation_score, comment_timestamp
