ddd37bdb684a560ce07860639ec338532f7e4447
wc-dev <chan.wilson.wc@gmail.com>
2023-06-11 09:35:17 -0700

    refactoring: removing duplicate code and adding in routes storage area in env file

--- backend/app/views/sentiment.py
+++ backend/app/views/sentiment.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, g
+from flask import Blueprint, request, jsonify, g, currentApp
 from ..services.reddit import RedditApp
 from ..services.sentiment_analysis import (
     getPostSentiment,
@@ -10,14 +10,7 @@ from ..database.database_handler import DatabaseHandler
 sentiment = Blueprint("sentiment", __name__)
 
 
-@sentiment.route("/api/sentiment/test", methods=["POST"])
-def analyzePostSentiment():
-    # add check to see if submission is already in database
-    # do a num comments difference
-    redditApp = RedditApp(g.reddit)
-    db = DatabaseHandler(g.db)
-    postURL = request.json.get("postURL")
-    results = []
+def analyze_and_store_sentiments(postURL, redditApp, db):
     commentsTimed = redditApp.getPostComments(postURL)
     submission = redditApp.reddit.submission(postURL)
 
@@ -56,47 +49,23 @@ def analyzePostSentiment():
     return jsonify(post=submission.title, comments=results, postURL=postURL)
 
 
-@sentiment.route("api/sentiment/initial", methods=["POST"])
+@sentiment.route(currentApp.config["POST_SENTIMENT_ANALYSIS"], methods=["POST"])
+def analyzePostSentiment():
+    redditApp = RedditApp(g.reddit)
+    db = DatabaseHandler(g.db)
+    postURL = request.json.get("postURL")
+
+    results = analyze_and_store_sentiments(postURL, redditApp, db)
+
+    return results
+
+
+@sentiment.route(currentApp.config["POST_SENTIMENT_ANALYSIS_INITIAL"], methods=["POST"])
 def initialRandomSentiment():
     redditApp = RedditApp(g.reddit)
     db = DatabaseHandler(g.db)
     postURL = redditApp.getRandomSubmission()
-    results = []
-    id = ""
-    commentsTimed = redditApp.getPostComments(postURL)
-    submission = redditApp.reddit.submission(postURL)
-    title_sentiment, content_sentiment = getPostSentiment(
-        submission.title, submission.selftext
-    )
-    post_sentiment = calcPostSentiment(title_sentiment, content_sentiment)
-    summation_score = post_sentiment["summation_score"]
-    db.call_store_post(
-        submission,
-        post_sentiment["post_compound"],
-        post_sentiment["post_neg"],
-        post_sentiment["post_neu"],
-        post_sentiment["post_pos"],
-        post_sentiment["summation_score"],
-    )
 
-    for i, comments in enumerate(commentsTimed):
-        sentiment_score = getCommentSentiment(comments[0].body)
-        summation_score = summation_score + float(sentiment_score["compound"])
-        id = comments[0].submission.id
-        db.call_store_comment_sentiment(
-            comments[0].submission.id,
-            sentiment_score["compound"],
-            sentiment_score["neg"],
-            sentiment_score["neu"],
-            sentiment_score["pos"],
-            summation_score / (i + 2),
-            comments[1],
-        )
+    results = analyze_and_store_sentiments(postURL, redditApp, db)
 
-    results = db.call_get_comment_sentiments(id)
-
-    return jsonify(
-        post=submission.title,
-        comments=results,
-        postURL=postURL,
-    )
+    return results
