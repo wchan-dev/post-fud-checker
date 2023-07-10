a9b6cc668cf93a9f014e510aea10abb8f74a874d
wc-dev <chan.wilson.wc@gmail.com>
2023-07-10 13:58:42 -0700

    feat: line colors will change depending if decreasing or increasing

--- frontend/src/components/SentimentPlot/SentimentPlot.tsx
+++ frontend/src/components/SentimentPlot/SentimentPlot.tsx
@@ -61,121 +61,64 @@ const CommentSentimentPlot: React.FC<CommentSentimentPlotProps> = ({
       },
     ];
   } else {
-    data = [
-      {
-        x: timeStamps,
-        y: sentiments,
-        mode: "line",
-      },
-    ];
-  }
-  let layout;
+    const decreasingSentiments = [];
+    const increasingSentiments = [];
 
-  if (plotType === "line") {
-    layout = {
-      autosize: true,
-      plot_bgcolor: bgColor,
-      paper_bgcolor: bgColor,
-      font: {
-        family: "Inter, sans-serif",
-        size: 12,
-        color: textColor,
-      },
-      title: {
-        text: "Comment Sentiment Over Time",
-        font: {
-          size: 14,
-        },
-        x: 0.5,
-      },
-      xaxis: {
-        title: "Time",
-        showgrid: false,
-        zeroline: true,
-        rangeselector: {
-          x: 0.08,
-          buttons: [
-            {
-              count: 1,
-              label: "1d",
-              step: "day",
-              stepmode: "backward",
-            },
-            {
-              count: 5,
-              label: "5d",
-              step: "day",
-              stepmode: "backward",
-            },
-            {
-              count: 1,
-              label: "1m",
-              step: "month",
-              stepmode: "backward",
-            },
-            { step: "all" },
-          ],
-        },
-      },
-      yaxis: {
-        title: "Sentiment",
-        showline: false,
-      },
-      updatemenus: [
-        {
-          x: 0.07,
-          y: 1.1,
-          showactive: false,
-          buttons: [
-            {
-              label: "Lines",
-              method: "restyle",
-              args: ["mode", "lines", [0]], // Trace index added in arguments list
-            },
-            {
-              label: "Markers",
-              method: "restyle",
-              args: ["mode", "markers", [0]], // Trace index added in arguments list
-            },
-            {
-              label: "Lines+Markers",
-              method: "restyle",
-              args: ["mode", "lines+markers", [0]], // Trace index added in arguments list
-            },
-          ],
-        },
-      ],
-      margin: { l: 50, r: 20, b: 50, t: 50, pad: 4 },
-    };
-  } else {
-    layout = {
-      autosize: true,
-      plot_bgcolor: bgColor,
-      paper_bgcolor: bgColor,
-      font: {
-        family: "Inter, sans-serif",
-        size: 12,
-        color: textColor,
-      },
-      title: {
-        text: "Frequency Distribution of Comment Sentiment",
-        font: {
-          size: 14,
-        },
-        x: 0.5,
-      },
-      xaxis: {
-        title: "Sentiment Score",
-        showgrid: false,
-        zeroline: true,
-      },
-      yaxis: {
-        title: "Comment Count",
-        showline: false,
-      },
-      margin: { l: 50, r: 20, b: 50, t: 50, pad: 4 },
-    };
+    for (let i = 1; i < timeStamps.length; i++) {
+      if (sentiments[i] >= sentiments[i - 1]) {
+        increasingSentiments.push({
+          x: [timeStamps[i - 1], timeStamps[i]],
+          y: [sentiments[i - 1], sentiments[i]],
+          mode: "lines",
+          line: { color: "green" },
+          name: "Increasing",
+        });
+      } else {
+        decreasingSentiments.push({
+          x: [timeStamps[i - 1], timeStamps[i]],
+          y: [sentiments[i - 1], sentiments[i]],
+          mode: "lines",
+          line: { color: "red" },
+          name: "Decreasing",
+        });
+      }
+    }
+
+    data = [...increasingSentiments, ...decreasingSentiments];
   }
+
+  // Add layout
+  const layout = {
+    autosize: true,
+    showlegend: false,
+    plot_bgcolor: bgColor,
+    paper_bgcolor: bgColor,
+    font: {
+      family: "Inter, sans-serif",
+      size: 12,
+      color: textColor,
+    },
+    title: {
+      text:
+        plotType === "histogram"
+          ? "Frequency Distribution of Comment Sentiment"
+          : "Comment Sentiment Over Time",
+      font: {
+        size: 14,
+      },
+      x: 0.5,
+    },
+    xaxis: {
+      title: plotType === "histogram" ? "Sentiment Score" : "Time",
+      showgrid: false,
+      zeroline: true,
+    },
+    yaxis: {
+      title: plotType === "histogram" ? "Comment Count" : "Sentiment",
+      showline: false,
+    },
+    margin: { l: 50, r: 20, b: 50, t: 50, pad: 4 },
+  };
 
   return (
     <Box w="100%" height="600px" minW="864px">
